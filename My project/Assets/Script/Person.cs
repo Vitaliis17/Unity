a89012a3f0f3966566c41a893888dabb59ada6f3
Vitaliis <Vitalik12-14@mail.ru>
2025-01-06 16:11:56 +0300

    xcc
    xc

---
+++ My project/Assets/Script/Person.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("sfa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
