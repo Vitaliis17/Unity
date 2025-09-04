34f8d4923030a1bfb653af61a09abea1c23bb391
Vitalik <Vitalik12-14@mail.ru>
2025-09-04 11:48:17 +0300

    Remove class Pool and transfer in Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Pool _pool;
-
     [SerializeField] private float _positionY;
 
     [SerializeField] private float _minPositionX;
@@ -12,9 +11,17 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float _minPositionZ;
     [SerializeField] private float _maxPositionZ;
 
+    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private Transform _folder;
+
+    private ObjectPool<Cube> _cubes;
+
+    private void Awake()
+        => _cubes = new ObjectPool<Cube>(Create, Get, Release, Destroy);
+
     public Cube Spawn()
     {
-        Cube cube = _pool.GetObject();
+        Cube cube = _cubes.Get();
 
         float positionX = Random.Range(_minPositionX, _maxPositionX);
         float positionZ = Random.Range(_minPositionZ, _maxPositionZ);
@@ -24,6 +31,18 @@ public class Spawner : MonoBehaviour
         return cube;
     }
 
-    public void Release(Cube cube)
-        => _pool.ReleaseCube(cube);
+    public void ReleaseCube(Cube cube)
+        => _cubes.Release(cube);
+
+    private Cube Create()
+        => Instantiate(_cubePrefab, _folder.transform).GetComponent<Cube>();
+
+    private void Get(Cube cube)
+        => cube.gameObject.SetActive(true);
+
+    private void Release(Cube cube)
+        => cube.gameObject.SetActive(false);
+
+    private void Destroy(Cube cube)
+        => Destroy(cube.gameObject);
 }
